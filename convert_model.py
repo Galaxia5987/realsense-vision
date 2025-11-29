7311c75846119def889bce22fdcbbf095ec448e7
AdarWa <adar.wasserman@gmail.com>
2025-11-29 15:57:39 +0200

    Use `yolo` shell command instead of python to display detailed log

--- convert_model.py
+++ convert_model.py
@@ -1,15 +1,30 @@
 import asyncio
 import os
-from ultralytics import YOLO
+import subprocess
+import sys
+
+realtime = []
 
 def convert_model(model_path, chip='rk3588'):
-    os.chdir(os.path.dirname(model_path))
-    model = YOLO(model_path)
-    
-    out_path = model.export(format='rknn', name=chip)
-    os.chdir('..')
-    return out_path
+    global realtime
 
+    cwd = os.path.dirname(model_path)
+    model_file = os.path.basename(model_path)
+
+    cmd = [os.path.dirname(sys.executable) + "/yolo", 'export', f'model={model_file}', 'format=rknn', f'name={chip}']
+
+    # Run CLI and capture stdout line by line
+    with subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1) as process:
+        for line in process.stdout: # type: ignore
+            realtime.append(line)
+            print(line, end='') 
+
+        process.wait()
+
+def reset_realtime():
+    global realtime
+    realtime = []
+        
 async def async_convert_model(model_path, chip='rk3588s'):
     await asyncio.to_thread(
         convert_model,
