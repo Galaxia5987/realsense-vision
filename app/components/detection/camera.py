53a2d588a7306943f03ddfd9e9643348650e8753
Adar W <adar.wasserman@gmail.com>
2025-12-03 11:23:39 +0000

    configure depth sensor properly

--- app/components/detection/camera.py
+++ app/components/detection/camera.py
@@ -135,7 +135,8 @@ class RealSenseCamera(AsyncLoopBase):
 
             depth_sensor = profile.get_device().first_depth_sensor()
             try:
-                depth_sensor.set_option(rs.option.visual_preset, 3)
+                depth_sensor.set_option(rs.option.visual_preset, 1)
+                depth_sensor.set_option(rs.option.laser_power, 360)
             except Exception as e:
                 logger.error(f"Exception while loading preset config onto camera: {e}", exc_info=True)
 
